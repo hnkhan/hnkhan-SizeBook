954cdc4770a152937e5ea13597bfe75cd002fb6d
Hussain Khan <hnkhan@cs.ualberta.ca>
2017-01-24 14:07:36 -0700

    Added list view and two buttons

--- app/src/main/java/com/example/hnkhan/hnkhan_sizebook/MainActivity.java
+++ app/src/main/java/com/example/hnkhan/hnkhan_sizebook/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.hnkhan.hnkhan_sizebook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView recordsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recordsList = (ListView) findViewById(R.id.recordsList);
     }
 }
